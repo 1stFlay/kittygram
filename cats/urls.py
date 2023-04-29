b1b62ac236a9210c484bab31b8df9e60aff6d6d6
1stFlay <samaraqwerty63@gmail.com>
2023-04-29 14:17:02 +0400

    завершил урок от 20.04.2023

--- cats/urls.py
+++ cats/urls.py
@@ -1,8 +1,31 @@
-from django.urls import path
+# ## 1)просто код( ниже через дженерики)
+# # from django.urls import path
 
-from .views import cat_list, cat_detail, CatListApi
+# # from .views import cat_list, cat_detail, CatListApi
+
+# # urlpatterns = [
+# #     path('cats/', CatListApi.as_view()),
+# #     path('cats/<int:pk>/', cat_detail),
+# # ]
+
+# ## 2) Еще
+# from django.urls import path
+
+# from cats.views import CatList, CatDetail
+
+# urlpatterns = [
+#     path('cats/', CatList.as_view()),
+#     path('cats/<int:pk>/', CatDetail.as_view()),
+# ]
+from django.urls import path, include
+
+from .views import CatViewSet, OwnerViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
 
 urlpatterns = [
-    path('cats/', CatListApi.as_view()),
-    path('cats/<int:pk>/', cat_detail),
+    path('', include(router.urls)),
 ]
